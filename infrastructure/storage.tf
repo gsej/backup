a53569e442a3900b30ed981990370562cbe963c3
gsej <gsej@erskinejones.com>
2023-03-13 22:14:09 +0000

    exclude bash scripts until secrets can be removed

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -1,14 +1,22 @@
- locals {
-   storage_account_name = "gsejstorageaccount1"
-   storage_account_container = "test1"
- }
+locals {
+  storage_account_name      = "gsejstorageaccount1"
+  storage_account_container = "test1"
+}
 
- resource "azurerm_storage_account" "storage-account" {
-   name                = local.storage_account_name
-   resource_group_name = azurerm_resource_group.group.name
-   location = azurerm_resource_group.group.location
-   account_tier = "Standard"
-   account_replication_type = "LRS"
+resource "azurerm_storage_account" "storage-account" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.group.name
+  location                 = azurerm_resource_group.group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Cool"
+
+  blob_properties {
+    last_access_time_enabled = true
+    delete_retention_policy {
+      days = 100
+    }
+  }
 }
 
 
